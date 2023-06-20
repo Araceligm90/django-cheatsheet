e5fcea1874a4a5bd7dd1846d821baf2ba9617ce0
Araceligm90 <garcia.araceli90@gmail.com>
2023-06-20 11:57:15 +0200

    finished guide

---
+++ django_things_project/things/urls.py
@@ -0,0 +1,11 @@
+# Here we define our routes
+from django.urls import path
+
+from .views import HomePageView, AboutView
+
+
+# urlpatterns <-- named specifically for Django
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about', AboutView.as_view(), name='about')
+]
